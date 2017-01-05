7661ab26a6b0ef7b564635e826acf71ca4bc54bc
Minsheng <shuangrain07@gmail.com>
2017-01-05 20:00:45 +0800

    完成迴圈版。

--- RecursiveEample/RecursiveEample/ForLoop.cs
+++ RecursiveEample/RecursiveEample/ForLoop.cs
@@ -11,22 +11,67 @@ namespace RecursiveEample
 
         public Int64 Iterative(Int64 n)
         {
-            throw new NotImplementedException();
+            int ans = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                ans += i;
+            }
+
+            return ans;
         }
 
         public Int64 AdvancedIterative(Int64 n)
         {
-            throw new NotImplementedException();
+            int ans = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    ans -= i;
+                }
+                else
+                {
+                    ans += i;
+                }
+            }
+
+            return ans;
         }
 
         public Int64 Factorial(Int64 n)
         {
-            throw new NotImplementedException();
+            int ans = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                ans *= i;
+            }
+
+            return ans;
         }
 
         public Int64 Fibonacci(Int64 n)
         {
-            throw new NotImplementedException();
+            int ans = 0;
+            int[] arr = new int[2] { 1, 1 };
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i < 3)
+                {
+                    ans = 1;
+                }
+                else
+                {
+                    ans = arr[0] + arr[1];
+                    arr[0] = arr[1];
+                    arr[1] = ans;
+                }
+            }
+
+            return ans;
         }
     }
 }
